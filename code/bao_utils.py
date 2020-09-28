0c79bdafb8751d9b78eea3dbbc0ec46e34ab18b4
Kate Storey-Fisher <kstoreyfisher@gmail.com>
2020-09-28 13:30:26 -0400

    update scripts for running est

--- code/bao_utils.py
+++ code/bao_utils.py
@@ -2,59 +2,129 @@ import numpy as np
 import math
 import glob
 
+from scipy.optimize import curve_fit
 import nbodykit
 from nbodykit.lab import *
 
 
 
 # cosmo and cosmo_fid are nbodykit cosmology objects
-def compute_alpha(z, cosmo, cosmo_fid):
+def compute_alpha(z, cosmo_truth, cosmo_fid):
 
-    cosmoa = cosmo.to_astropy()
+    cosmoa_truth = cosmo_truth.to_astropy()
     cosmoa_fid = cosmo_fid.to_astropy()
 
-    da = cosmoa.angular_diameter_distance(z)
+    da = cosmoa_truth.angular_diameter_distance(z) #returns D_A in Mpc
     da_fid = cosmoa_fid.angular_diameter_distance(z)
 
-    H = cosmoa.H(z)
+    H = cosmoa_truth.H(z)
     H_fid = cosmoa_fid.H(z)
 
-    rs = radius_sound_horizon(cosmo.Omega0_cdm, cosmo.Omega0_b, cosmo.h)
-    rs_fid = radius_sound_horizon(cosmo_fid.Omega0_cdm, cosmo_fid.Omega0_b, cosmo_fid.h)
+    
+    #Omega0_m_truth = cosmo_truth.Omega0_cdm + cosmo_truth.Omega0_b
+    #Omega0_m_fid = cosmo_fid.Omega0_cdm + cosmo_fid.Omega0_b
+    # this is the radius of the sound horizon at the baryon drag epoch, so don't pass z
+    #print(cosmo_truth.Omega0_m, cosmo_truth.Omega0_cdm, cosmo_truth.Omega0_b, cosmo_truth.h)
+    #print(cosmo_fid.Omega0_m, cosmo_fid.Omega0_cdm, cosmo_fid.Omega0_b, cosmo_fid.h)
+    rs = compute_radius_sound_horizon(cosmo_truth.Omega0_m, cosmo_truth.Omega0_b, cosmo_truth.h)
+    rs_fid = compute_radius_sound_horizon(cosmo_fid.Omega0_m, cosmo_fid.Omega0_b, cosmo_fid.h)
 
-    if z>0:
-        alpha = (da/da_fid)**(2./3.) * (H_fid/H)**(1./3.) * rs_fid/rs
+    rs_col = compute_rs(cosmo_truth.Omega0_m, cosmo_truth.Omega0_b, cosmo_truth.h)
+    rs_fid_col = compute_rs(cosmo_fid.Omega0_m, cosmo_fid.Omega0_b, cosmo_fid.h)
+
+    if z>0.0:
+        alpha = (da/da_fid)**(2./3.) * (H_fid/H)**(1./3.) * (rs_fid/rs)
+        alpha_col = (da/da_fid)**(2./3.) * (H_fid/H)**(1./3.) * (rs_fid_col/rs_col)
     else:
         #ignore angular diam dist?? 
-        alpha = (H_fid/H)**(1./3.) * rs_fid/rs
+        alpha = (H_fid/H)**(1./3.) * (rs_fid/rs)
 
-    print("Cosmo: r_s:", rs, "D_A:", da, "H:", H)
+    print("Truth: r_s:", rs, "D_A:", da, "H:", H)
     print("Fiducial: r_s:", rs_fid, "D_A:", da_fid, "H:", H_fid)
+    #print("rs_truth_col", rs_col, "rs_fid_col", rs_fid_col)
     print("alpha:", alpha)
+    #print("alpha_col:", alpha_col)
 
     return alpha
 
 
+def compute_da(z, cosmo):
+    cosmoa = cosmo.to_astropy()
+    da = cosmoa.angular_diameter_distance(z)
+    return da
+
+def compute_H(z, cosmo):
+    cosmoa = cosmo.to_astropy()
+    H = cosmoa.H(z)
+    return H
+
+
 def get_cosmo(cosmo_name):
-    cosmo_names = ['planck', 'b17']
+    cosmo_names = ['planck', 'b17', 'wmap9', 'patchy']
     if cosmo_name=='planck':
         cosmo = nbodykit.cosmology.Planck15
+    elif cosmo_name=='wmap9':
+        cosmo = nbodykit.cosmology.WMAP9
+    # Kitaura 2016; PATCHY mocks as used in BOSS, from Big Multidark sim 
+    elif cosmo_name=='patchy':
+        h0 = 0.6777
+        m_ncdm = [] #pass empty list for no massive neutrinos
+
+        Omega0_m = 0.307115
+        Omega0_b = 0.048206
+        Omega0_cdm = round(Omega0_m-Omega0_b, 6)
+
+        ns = 0.9611
+        sigma_8 = 0.8288
+        cosmo = nbodykit.cosmology.Cosmology(h=h0, Omega0_b=Omega0_b, Omega0_cdm=Omega0_cdm, 
+                                                n_s=ns, m_ncdm=m_ncdm)
+        cosmo = cosmo.match(sigma8=sigma_8) 
     # Beutler et al 2017 (& Ross et al 2017), as used in Barry paper (Hinton 2019)
     elif cosmo_name=='b17':
-        Omega0_cdm = 0.31
-        Omega0_b = 0.04814
         h0 = 0.676
+        m_ncdm = [0.06]
+        rho_crit = 93.14 * h0**2 #in eV
+        Omega0_nu = np.sum(m_ncdm) / rho_crit
+
+        Omega0_m = 0.31 
+        Omega0_b = 0.022/(h0**2)
+        Omega0_cdm = round(Omega0_m-Omega0_b-Omega0_nu, 6)
+        
         ns = 0.96
-        cosmo = nbodykit.cosmology.Cosmology(h=h0, Omega0_b=Omega0_b, Omega0_cdm=Omega0_cdm, n_s=ns)
+        sigma_8 = 0.824
+        cosmo = nbodykit.cosmology.Cosmology(h=h0, Omega0_b=Omega0_b, Omega0_cdm=Omega0_cdm, 
+                                                n_s=ns, m_ncdm=m_ncdm)
+        cosmo = cosmo.match(sigma8=sigma_8)
     else:
         print("Name not recognized! Must be one of the following:", cosmo_names)
         return      
+    print(cosmo.Omega0_cdm, cosmo.Omega0_m, cosmo.Omega0_b, cosmo.h, cosmo.n_s, cosmo.m_ncdm, cosmo.sigma8, cosmo.N_ur)
     return cosmo
 
 
+def nbodykit_to_lognormal_galaxies(cosmo):
+    print("oc0h2 =", round(cosmo.Omega0_cdm * cosmo.h**2, 6), '  # \Omega_c h^2')
+    print("mnu =", float(sum(cosmo.m_ncdm)), r'  # \Sigma m_{\nu} total neutrino mass')
+    print("ns =", cosmo.n_s)
+    print("lnAs =", round(np.log(cosmo.A_s/1e-10), 6))
+    print("ob0h2 =", round(cosmo.Omega0_b * cosmo.h**2, 6), '  #\Omega_baryon h^2')
+    print("h0 =", cosmo.h, '  # H0/100')
+    print("w = -1")
+    print("run = 0.0   # running index of pk")
+
+
+# The sound horizon at recombination in Mpc (Mpc to be consistent with D_A and H)
+# From Colossus: https://bitbucket.org/bdiemer/colossus/src/master/colossus/cosmology/cosmology.py
+def compute_rs(om, ob, h0):
+    obh2 = ob * h0 * h0
+    omh2 = om * h0 * h0
+    r_s = 44.5 * np.log(9.83 / omh2) / np.sqrt(1.0 + 10.0 * obh2**0.75)
+    return r_s
+    
+
 # Compute the Eisenstein and Hu 1998 value for the sound horizon
 # from Barry (Hinton 2019): https://github.com/Samreay/Barry/blob/c34645ca56bed749d1480ba0e40d495fe5a96574/barry/cosmology/power_spectrum_smoothing.py
-def radius_sound_horizon(om, ob, h0):
+def compute_radius_sound_horizon(om, ob, h0):
 
     # Fitting parameters
     b1 = 0.313
@@ -87,31 +157,77 @@ def radius_sound_horizon(om, ob, h0):
     return s
 
 
-def calc_alpha(cat_tag, cf_tag, realizations=range(100)):
+def get_alphas(cat_tag, cf_tag, realizations=range(100)):
     cat_dir = '../catalogs'
     result_dir = '../results/results_lognormal{}'.format(cat_tag)
     
-    xis = []
     n_converged = 0
-    alphas = []
+    alphas = np.full(np.array(realizations).shape, np.NaN)
+
+    if 'baoiter' not in cf_tag:
+        raise ValueError("Need baoiter in cf_tag! Passed cf_tag={cf_tag}")
+        return
+
+    i = 0
     for Nr in realizations:
 
-        if 'baoiter' in cf_tag:
-            fn_pattern = f"cf{cf_tag}_converged_*{cat_tag}_rlz{Nr}.npy"
-            for cf_fn in glob.glob(f'{result_dir}/{fn_pattern}'):
-                #print(cf_fn)
-                r_avg, xi, amps, _, extra_dict = np.load(cf_fn, allow_pickle=True)
-                #print("C:", amps[4])
-                alphas.append(extra_dict ['alpha_result'])
-                n_converged +=1
-                break #should only be 1 match; but probs better way to do this
-        else:
-            cf_fn = '{}/cf{}{}_rlz{}.npy'.format(result_dir, cf_tags[i], cat_tag, Nr)
-            r_avg, xi, amps = np.load(cf_fn, allow_pickle=True)
+        fn_pattern = f"cf{cf_tag}_converged_*{cat_tag}_rlz{Nr}.npy"
+        for cf_fn in glob.glob(f'{result_dir}/{fn_pattern}'):
+            #print(cf_fn)
+            r_avg, xi, amps, _, extra_dict = np.load(cf_fn, allow_pickle=True)
+            #print("C:", amps[4])
+            alphas[i] = extra_dict['alpha_result']
+            if extra_dict ['alpha_result'] < 0.5 or extra_dict ['alpha_result'] > 1.5:
+                print(Nr, extra_dict ['alpha_result'])
+            n_converged +=1
+            break #should only be 1 match; but probs better way to do this
+        i += 1
 
-    print(cf_tag)
     print(f"Found {n_converged} converged BAO cfs ({len(realizations)-n_converged} not converged)")    
-    print("alpha_mean:", np.mean(alphas))
-    print("alpha_std:", np.std(alphas))
+    print("alpha_mean:", np.nanmean(alphas))
+    print("alpha_median:", np.nanmedian(alphas))
+    print("alpha_std:", np.nanstd(alphas))
     return alphas
 
+
+def make_bao_fit(cf_func):
+    
+    def bao_fit(s, a1, a2, a3, bsq, alpha):
+        b1, b2, b3, b4 = bao_bases(s, cf_func, alpha=alpha)
+        return a1*b1 + a2*b2 + a3*b3 + bsq*b4
+    
+    return bao_fit
+
+
+def bao_bases(s, cf_func, alpha=1):
+
+    b1 = 1.0/s**2
+    b2 = 0.1/s
+    b3 = 0.001*np.ones(len(s))
+
+    cf = cf_func(s*alpha)
+    b4 = cf
+
+    return b1, b2, b3, b4
+
+
+
+def bao_fit_standard(r_arr, xi_arr, cosmo_base, redshift=0.0, bias=1.0, realizations=range(100)):
+    Plin = cosmology.LinearPower(cosmo_base, redshift, transfer='EisensteinHu')
+    CF = cosmology.correlation.CorrelationFunction(Plin)
+    def cf_model(s):
+        return bias * CF(s)
+
+    alphas = np.zeros(len(realizations))
+    for Nr in realizations:
+        r_points = r_arr[Nr]
+        xi_points = xi_arr[Nr]
+        try:
+            popt, _ = curve_fit(make_bao_fit(cf_model), r_points, xi_points)
+            a1, a2, a3, bsq, alpha = popt
+            alphas[Nr] = alpha
+        except RuntimeError:
+            print(f"Optimal parameters for realization {Nr} not found! continuing")
+            alphas[Nr] = np.nan
+
+    return alphas
--- code/cf_test.py
+++ code/cf_test.py
@@ -2,6 +2,8 @@ import numpy as np
 import Corrfunc
 from Corrfunc.io import read_lognormal_catalog
 from Corrfunc.theory import DDsmu
+from Corrfunc.utils import evaluate_xi
+from Corrfunc.utils import qq_analytic
 
 print("smoothcorrfunc file:", Corrfunc.__file__)
 print("smoothcorrfunc version:", Corrfunc.__version__)
@@ -27,4 +29,13 @@ dd_res, dd_proj, _ = DDsmu(1, nthreads, r_edges, mumax, nmubins, x, y, z,
                            boxsize=boxsize, periodic=periodic, proj_type=proj_type, nprojbins=nprojbins)
 print("Corrfunc result:", dd_res)
 print("Smoothcorrfunc result:", dd_proj)
+
+volume = boxsize**3
+rr_ana, qq_ana = qq_analytic(rmin, rmax, nd, volume, nprojbins, proj_type, rbins=r_edges)
+
+numerator = dd_proj - rr_ana
+amps_ana, *_ = np.linalg.lstsq(qq_ana, numerator, rcond=None) # Use linalg.lstsq instead of actually computing inverse!
+r_fine = np.linspace(rmin, rmax, 200)
+xi_ana = evaluate_xi(amps_ana, r_fine, proj_type, rbins=r_edges)
+print(xi_ana)
 print("Successfully ran smoothcorrfunc on lognormal mock!")
